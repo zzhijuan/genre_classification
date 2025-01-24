f42dadd1b52dbf116ae7dbca7e237504daf2de85
zhijuan zhang <zhijuanzhang@zhijuans-MacBook-Air.local>
2025-01-24 15:38:06 -0600

    starter kit

---
+++ lesson-2-data-exploration-and-preparation/exercises/exercise_5/starter/run.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+import argparse
+import logging
+import pandas as pd
+import wandb
+
+
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
+
+
+def go(args):
+
+    run = wandb.init(project="exercise_5", job_type="process_data")
+
+    ## YOUR CODE HERE
+    artifact = run.use_artifact(args.input_artifact)
+    artifact_path = artifact.file()
+
+    df = pd.read_parquet(artifact_path)
+    df.drop_duplicates().reset_index(drop=True)
+    df['title'].fillna(value='', inplace=True)
+    df['song_name'].fillna(value='', inplace=True)
+    df['text_feature'] = df['title'] + ' ' + df['song_name']
+
+    # save results
+    df.to_csv('preprocessed_data.csv')
+
+    # artifact
+    artifact_new = wandb.Artifact(name = args.artifact_name, type = args.artifact_type, description = args.artifact_description)
+    artifact_new.add_file('preprocessed_data.csv')
+    run.log_artifact(artifact_new)
+    run.finish()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Preprocess a dataset",
+        fromfile_prefix_chars="@",
+    )
+
+    parser.add_argument(
+        "--input_artifact",
+        type=str,
+        help="Fully-qualified name for the input artifact",
+        required=True,
+    )
+
+    parser.add_argument(
+        "--artifact_name", type=str, help="Name for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_type", type=str, help="Type for the artifact", required=True
+    )
+
+    parser.add_argument(
+        "--artifact_description",
+        type=str,
+        help="Description for the artifact",
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    go(args)
